6d16b93cfbed245948b6fa1efeacb9c01b0a0255
FYUDerma <zakaria.12ans@gmail.com>
2024-07-23 10:20:49 +0200

    Adding the format prototype in the header file

--- main.h
+++ main.h
@@ -7,8 +7,16 @@
 #include <string.h>
 #include <ctype.h>
 #include <stdlib.h>
+#include <stdarg.h>
 
 /* Prototype */
 int _printf(const char *format, ...);
+
+/* Format Prototype */
+int print_char(va_list list);
+int print_string(va_list list);
+int print_percent(va_list list);
+int print_integer(va_list list);
+int unsigned_integer(va_list list);
 
 #endif
