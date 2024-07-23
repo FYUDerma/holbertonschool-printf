9482160b2a2b07f712a36eea44ff5caabd6f92b3
FYUDerma <zakaria.12ans@gmail.com>
2024-07-23 10:19:50 +0200

    Making the putchar function

---
+++ _putchar.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _putchar - writes the caracter c
+ * @c: the caracter to print
+ *
+ * return: On succes 1.
+ * On error, -1 is returned, and errno is set to appropriately
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
