9abc59f47f6099898222a45fe0bb6441889c948d
Binchat <binbin.he515@outlook.com>
2024-07-26 14:56:32 +0200

    update printnumber file

--- main.h
+++ main.h
@@ -15,13 +15,11 @@ int _putchar(char c);
 int _printf(const char *format, ...);
 int (*get_format_func(char *specifier))(va_list);
 
-
 /* Format Prototype */
 int print_char(va_list list);
 int print_str(va_list list);
 int print_percent(va_list list);
 int print_integer(va_list list);
-int print_int(va_list list);
 int print_number(int num);
 
 #endif
--- print_number.c
+++ print_number.c
@@ -1,33 +1,6 @@
 #include "main.h"
 
 /**
- * print_int - Prints an integer
- * @list: list of arguments
- * Return: return the amount of characters
- */
-int print_int(va_list list)
-{
-	int num = va_arg(list, int);
-	int len = 0;
-
-	if (num == 0)
-	{
-		_putchar('0');
-		return (1);
-	}
-
-	if (num < 0)
-	{
-		_putchar('-');
-		len++;
-		num = -num;
-	}
-
-	len += print_number(num);
-	return (len);
-}
-
-/**
  * print_number - Prints a number
  * @num: The number to print
  * Return: The number of characters printed
